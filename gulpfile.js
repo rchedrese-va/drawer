f1c56a58a8d89f1c68dbc47c14ac896596068252
Raul Chedrese <rchedrese@vendasta.com>
2014-12-03 11:59:57 -0600

    swtiched to gulp and sass.

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-ruby-sass');
+var reload = browserSync.reload;
+
+gulp.task('default', function() {
+  // place code for default task.
+  console.log('Gulp is running!');
+})
+
+// Static server
+gulp.task('watch', function() {
+  browserSync({
+    notify: false,
+    server: {
+      baseDir: "./src/"
+    }
+  });
+
+  gulp.watch(['src/sass/*.scss'], ['styles', reload]);
+  gulp.watch(['src/*.html'], reload);
+  gulp.watch(['src/js/*.js'], reload);
+});
+
+gulp.task('copy', ['styles', 'js'], function() {
+  return;
+});
+
+// Compile Sass files
+gulp.task('styles', function() {
+  return gulp.src('src/sass/*.scss')
+    .pipe(sass({sourcemap: true}))
+    .pipe(gulp.dest('dist/css')) // Write the CSS & Source maps
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('js', function() {
+  return gulp.src(['src/js/jquery.drawer.js'])
+    .pipe(gulp.dest('dist/js'));
+});
+
