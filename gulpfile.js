4dd7eb0e265cd80f4a8301f2dc40211b23c0bd90
Raul Chedrese <rchedrese@vendasta.com>
2014-12-09 11:30:32 -0600

    added tablet layout.

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('watch', function() {
   browserSync({
     notify: false,
     server: {
-      baseDir: "./"
+      baseDir: "./docs"
     }
   });
 
@@ -29,13 +29,21 @@ gulp.task('copy', ['styles', 'js'], function() {
 // Compile Sass files
 gulp.task('styles', function() {
   return gulp.src('src/sass/*.scss')
-    .pipe(sass({sourcemap: true}))
+    .pipe(sass())
     .pipe(gulp.dest('dist/css')) // Write the CSS & Source maps
+    .pipe(gulp.dest('docs/css'))
     .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('styles:docs', function() {
+  return gulp.src('docs/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('docs/css'));
 });
 
 gulp.task('js', function() {
   return gulp.src(['src/js/jquery.drawer.js'])
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('docs/js'));
 });
 
